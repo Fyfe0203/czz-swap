6bbb6e4aff03b17adf1da6711870f637636e2b84
Baker-Martin <Baker-Martin@protonmail.com>
2021-04-17 21:09:25 +0800

    update network and pools

--- src/context/pools.js
+++ src/context/pools.js
@@ -21,100 +21,157 @@ const pools = [
     image:'https://docs.classzz.com/svg/BSC.svg'
   },
   {
-    tokenAddress: "0x103C9FfFf6FBD142a1bb12F49E3387Bd7a211C74",
-    name:'WBTC',
-    symbol: "WBTC",
-    decimals: 8,
+    tokenAddress: "0x956f47f50a910163d8bf957cf5846d573e7f87ca",
+    name:'Fei USD',
+    symbol: "FEI",
+    decimals: 18,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/7598/thumb/wrapped_bitcoin_wbtc.png?1548822744"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x956F47F50A910163D8BF957Cf5846D573E7f87CA/logo.png"
   },
   {
-    tokenAddress: "0x1193e93d967eD48ACf6e5e208da7e2F4f9B12910",
-    name:'SUSHI',
-    symbol: "SUSHI",
-    decimals: 8,
-    systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/12271/thumb/512x512_Logo_no_chop.png?1606986688"
-  },
-  {
-    tokenAddress: "0xc251D7983fb00917DF80BC336692d53B6b085fD6",
-    name:'USDC',
+    tokenAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    name:'USD//C',
     symbol: "USDC",
-    decimals: 8,
+    decimals: 6,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/6319/thumb/USD_Coin_icon.png?1547042389"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png"
   },
   {
-    tokenAddress: "0x18f419C0B1AC87d42CEdDf2326311153c7ea3886",
-    name:'DAI',
-    symbol: "DAI",
+    tokenAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    name:'Tether USD',
+    symbol: "USDT",
+    decimals: 6,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png"
+  },
+  {
+    tokenAddress: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+    name:'Wrapped BTC',
+    symbol: "WBTC",
     decimals: 8,
     systemType: "ETH",
     image:"https://assets.coingecko.com/coins/images/9956/thumb/dai-multi-collateral-mcd.png?1574218774"
   },
   {
-    tokenAddress: "0xbBae92Ec8B158Bfce96E4d3Ee21Cca3aAA92849c",
-    name:'USDT',
-    symbol: "USDT",
-    decimals: 8,
+    tokenAddress: "0x66a0f676479cee1d7373f3dc2e2952778bff5bd6",
+    name:'Wise Token',
+    symbol: "WISE",
+    decimals: 18,
     systemType: "ETH",
     image:"https://assets.coingecko.com/coins/images/325/thumb/Tether-logo.png?1598003707"
   },
     {
-    tokenAddress: "0x568911F155c68c5b5eEfd10605916074168c0E56",
-    name:'WISE',
-    symbol: "WISE",
-    decimals: 8,
+    tokenAddress: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+    name:'Uniswap',
+    symbol: "UNI",
+    decimals: 18,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/13552/thumb/WISE-logo-1600x1280.png?1609727947"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984/logo.png"
   },
   {
-    tokenAddress: "0x03a3804E0B06d60596D3ff921F42c723bB14D81E",
-    name:'YFI',
-    symbol: "YFI",
-    decimals: 8,
+    tokenAddress: "0x5f98805a4e8be255a32880fdec7f6728c6568ba0",
+    name:'LUSD Stablecoin',
+    symbol: "LUSD",
+    decimals: 18,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/11849/thumb/yfi-192x192.png?1598325330"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x5f98805A4E8be255a32880FDeC7F6728C6568bA0/logo.png"
   },
   {
-    tokenAddress: "0x62Fa3e051Acd2fe0Bb7DCa4465bac2d0Cd45C0c2",
-    name:'LINK',
+    tokenAddress: "0x03ab458634910aad20ef5f1c8ee96f1d6ac54919",
+    name:'Rai Reflex Index',
+    symbol: "RAI",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x03ab458634910AaD20eF5f1C8ee96F1D6ac54919/logo.png"
+  },
+  {
+    tokenAddress: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    name:'Dai Stablecoin',
+    symbol: "DAI",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png"
+  },
+  {
+    tokenAddress: "0x514910771af9ca656af840dff83e8264ecf986ca",
+    name:'ChainLink Token',
     symbol: "LINK",
-    decimals: 8,
+    decimals: 18,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/877/thumb/chainlink-new-logo.png?1547034700"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x514910771AF9Ca656af840dff83E8264EcF986CA/logo.png"
   },
   {
-    tokenAddress: "0x743e211c78Bc00D615Fd3f6fB7C372ce5EDC719a",
-    name:'SNX',
-    symbol: "SNX",
-    decimals: 8,
-    systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/3406/thumb/SNX.png?1598631139"
-  },
-  {
-    tokenAddress: "0xf09885a2dc51bD8e34BF0072A483e1a25523e7Ee",
-    name:'CORE',
-    symbol: "CORE",
-    decimals: 8,
-    systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/12635/thumb/cvault.finance_logo.png?1601353499"
-  },
-  {
-    tokenAddress: "0xEc93eff8a951aa52B3ab659bEF27fac9b0a7F00B",
-    name:'DPI',
+    tokenAddress: "0x1494ca1f11d487c2bbe4543e90080aeba4ba3c2b",
+    name:'DefiPulse Index',
     symbol: "DPI",
-    decimals: 8,
+    decimals: 18,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/12465/thumb/defi_pulse_index_set.png?1600051053"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x1494CA1F11D487c2bBe4543E90080AeBa4BA3C2b/logo.png"
   },
   {
-    tokenAddress: "0x2135847305c331B7370A7Aba8a7b6699C885f170",
-    name:'FRAX',
+    tokenAddress: "0x62359ed7505efc61ff1d56fef82158ccaffa23d7",
+    name:'cVault.finance',
+    symbol: "CORE",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x62359Ed7505Efc61FF1D56fEF82158CcaffA23D7/logo.png"
+  },
+  {
+    tokenAddress: "0x853d955acef822db058eb8505911ed77f175b99e",
+    name:'Frax',
     symbol: "FRAX",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x853d955aCEf822Db058eb8505911ED77F175b99e/logo.png"
+  }, {
+    tokenAddress: "0x35a532d376ffd9a705d0bb319532837337a398e7",
+    name:'Wrapped DogeCoin',
+    symbol: "WDOGE",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://cryptologos.cc/logos/dogecoin-doge-logo.svg?v=010"
+  }, {
+    tokenAddress: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+    name:'Maker',
+    symbol: "MKR",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2/logo.png"
+  }, {
+    tokenAddress: "0xeef9f339514298c6a857efcfc1a762af84438dee",
+    name:'Hermez Network',
+    symbol: "HEZ",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xEEF9f339514298C6A857EfCfC1A762aF84438dEE/logo.png"
+  }, {
+    tokenAddress: "0xb62132e35a6c13ee1ee0f84dc5d40bad8d815206",
+    name:'Nexo',
+    symbol: "NEXO",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xB62132e35a6c13ee1EE0f84dC5d40bad8d815206/logo.png"
+  }, {
+    tokenAddress: "0x0000000000095413afc295d19edeb1ad7b71c952",
+    name:'Tokenlon',
+    symbol: "LON",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x0000000000095413afC295d19EDeb1Ad7B71c952/logo.png"
+  }, {
+    tokenAddress: "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9",
+    name:'Aave Token',
+    symbol: "AAVE",
+    decimals: 18,
+    systemType: "ETH",
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9/logo.png"
+  }, {
+    tokenAddress: "0xeb4c2781e4eba804ce9a9803c67d0893436bb27d",
+    name:'renBTC',
+    symbol: "renBTC",
     decimals: 8,
     systemType: "ETH",
-    image:"https://assets.coingecko.com/coins/images/13422/thumb/frax_logo.png?1608476506"
+    image:"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D/logo.png"
   },
   {
     tokenAddress: "0x9246Cd4bF4C6D520BAEaE347B4eC038DDB508211",
