ec37ec4e3434953b73f458b293cf5ed1806e115a
ninasaul <ninasaul@protonmail.com>
2021-04-20 23:42:20 +0800

    fixed: modal mask close

--- src/compontent/Modal.js
+++ src/compontent/Modal.js
@@ -5,9 +5,7 @@ export default function Modal(props) {
   const maskRef = useRef()
   const close = (e) => {
     e.preventDefault()
-     if (e.target === maskRef.current) {
-       maskClose && props.onClose(false)
-    }
+     if (e.target === maskRef.current)  maskClose && props.onClose(false)
   }
   return (
     <Fragment>
@@ -17,17 +15,17 @@ export default function Modal(props) {
           <div className="modal-wrap" onClick={close} ref={ maskRef } >
             <div className="modal" {...rest}  >
               <div className="ico-x modal-close" onClick={()=> props.onClose(false)}/>
-            {
-              title &&
-            <div className="f-c-sb modal-header">
-              <h2 className="modal-title">
-                {title}
-              </h2>
-            </div>
-            }
-            <div className="modal-content">
-              { props.children }
-            </div>
+                {
+                  title &&
+                <div className="f-c-sb modal-header">
+                  <h2 className="modal-title">
+                    {title}
+                  </h2>
+                </div>
+                }
+                <div className="modal-content">
+                  { props.children }
+                </div>
             </div>
           </div>
           <div className="mask f-c-c"/>
